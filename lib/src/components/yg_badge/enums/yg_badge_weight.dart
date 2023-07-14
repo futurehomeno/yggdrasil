f8a86af53c81746a3880fca048c90222d624df8e
Emerald <podbicanin.emerald@gmail.com>
2023-07-14 10:46:05 +0200

    yg badge mapper

--- lib/src/components/yg_badge/enums/yg_badge_weight.dart
+++ lib/src/components/yg_badge/enums/yg_badge_weight.dart
@@ -1,25 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:yggdrasil/yggdrasil.dart';
-
 enum YgBadgeWeight {
   weak,
   strong;
-
-  Color getBadgeColor(YgBadgeTheme theme) {
-    switch (this) {
-      case YgBadgeWeight.weak:
-        return theme.weakColor;
-      case YgBadgeWeight.strong:
-        return theme.strongColor;
-    }
-  }
-
-  TextStyle getTextStyle(YgBadgeTheme theme) {
-    switch (this) {
-      case YgBadgeWeight.weak:
-        return theme.weakTextStyle;
-      case YgBadgeWeight.strong:
-        return theme.strongTextStyle;
-    }
-  }
 }
