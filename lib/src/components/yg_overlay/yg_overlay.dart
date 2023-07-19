fdb80dd007ec3e0c2240bf2998e5dcaa9a7293b2
bjhandeland <bjarne@futurehome.no>
2023-07-19 11:28:14 +0200

    add new icons

---
+++ lib/src/components/yg_overlay/yg_overlay.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class YgOverlay extends StatelessWidget {
+  const YgOverlay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
