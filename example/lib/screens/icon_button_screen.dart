07540237b89741fb83193893f3b9bcaf279c0f00
bjhandeland <bjarne@futurehome.no>
2023-07-27 16:15:40 +0200

    icon button screen fix

--- example/lib/screens/icon_button_screen.dart
+++ example/lib/screens/icon_button_screen.dart
@@ -24,43 +24,51 @@ class IconButtonScreen extends StatelessWidget {
       child: Column(
         children: <Widget>[
           const YgListTile(title: 'Variants'),
+          const Text('Standard'),
           YgIconButton(
             onPressed: () {},
             child: const YgIcon(YgIcons.info),
           ),
+          const Text('Filled'),
           YgIconButton(
             onPressed: () {},
             variant: YgIconButtonVariant.filled,
             child: const YgIcon(YgIcons.info),
           ),
+          const Text('Tonal'),
           YgIconButton(
             onPressed: () {},
             variant: YgIconButtonVariant.tonal,
             child: const YgIcon(YgIcons.info),
           ),
+          const Text('Outlined'),
           YgIconButton(
             onPressed: () {},
             variant: YgIconButtonVariant.outlined,
             child: const YgIcon(YgIcons.info),
           ),
           const YgListTile(title: 'Sizes'),
+          const Text('Small'),
           YgIconButton(
             onPressed: () {},
             variant: YgIconButtonVariant.filled,
             size: YgIconButtonSize.small,
             child: const YgIcon(YgIcons.info),
           ),
+          const Text('Medium'),
           YgIconButton(
             onPressed: () {},
             variant: YgIconButtonVariant.filled,
             child: const YgIcon(YgIcons.info),
           ),
           const YgListTile(title: 'States'),
+          const Text('Enabled'),
           YgIconButton(
             onPressed: () {},
             variant: YgIconButtonVariant.filled,
             child: const YgIcon(YgIcons.info),
           ),
+          const Text('Disabled'),
           const YgIconButton(
             onPressed: null,
             variant: YgIconButtonVariant.filled,
