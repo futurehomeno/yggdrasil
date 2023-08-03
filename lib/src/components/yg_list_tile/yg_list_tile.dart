00218a80386c74208e979208c7d1fdce7658e6a4
bjhandeland <bjarne@futurehome.no>
2023-08-03 10:07:07 +0200

    added links factory

--- lib/src/components/yg_list_tile/yg_list_tile.dart
+++ lib/src/components/yg_list_tile/yg_list_tile.dart
@@ -21,6 +21,19 @@ class YgListTile extends StatelessWidget {
     this.onInfoTap,
   });
 
+  /// Convenience for generating links from YgListTiles.
+  factory YgListTile.link({
+    required String link,
+    required String iconPath,
+    required VoidCallback onTap,
+  }) {
+    return YgListTile(
+      title: link,
+      leadingWidgets: <Widget>[YgIcon(iconPath)],
+      onTap: onTap,
+    );
+  }
+
   final String title;
   final String? subtitle;
   final Widget? subtitleIcon;
