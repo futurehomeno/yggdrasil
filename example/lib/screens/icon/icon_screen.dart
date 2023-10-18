69d5323af2963ffc4c0705e047a6102649fd81a9
bjhandeland <bjarne@futurehome.no>
2023-10-18 13:21:28 +0200

    fix icon screen

--- example/lib/screens/icon/icon_screen.dart
+++ example/lib/screens/icon/icon_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yggdrasil/yggdrasil.dart';
 import 'package:yggdrasil_demo/core/_core.dart';
+import 'package:yggdrasil_demo/screens/icon_list/icon_list_screen.dart';
 import 'package:yggdrasil_demo/widgets/_widgets.dart';
 
 class IconScreen extends StatelessWidget {
@@ -17,20 +18,26 @@ class IconScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const DemoScreen(
+    return DemoScreen(
       componentName: 'Icon',
       componentDesc: 'Icons',
       supernovaLink: 'Link',
       child: Column(
         children: <Widget>[
-          YgSection.base(
+          YgSection.list(
             title: 'Available icons',
-            child: YgCallout(
-              description: 'All currently supported icons can be seen on the icons list screen.',
-            ),
+            subtitle: 'List of all available icons can be found on the IconListScreen.',
+            children: <YgListTile>[
+              YgListTile(
+                title: 'Icon list (all icons)',
+                onTap: () => sl<YgRouter>().push(IconListScreen.route()),
+                trailingWidgets: const <YgIcon>[YgIcon(YgIcons.caretRight)],
+              ),
+            ],
           ),
-          YgSection.base(
+          const YgSection.base(
             title: 'Sizes',
+            subtitle: 'Icons come in small and large.',
             child: Row(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
@@ -45,7 +52,7 @@ class IconScreen extends StatelessWidget {
               ],
             ),
           ),
-          YgSection.base(
+          const YgSection.base(
             title: 'Inherited color',
             subtitle: 'Color switching is handled by the parent widget.',
             child: YgTag(
@@ -57,33 +64,22 @@ class IconScreen extends StatelessWidget {
             ),
           ),
           YgSection.base(
-            title: 'Disabled color',
-            subtitle: 'Color switching for state is handled by the parent widget.',
-            child: YgButton(
-              onPressed: null,
-              child: YgIcon(
-                YgIcons.info,
-                size: YgIconSize.large,
-              ),
-            ),
-          ),
-          YgSection.base(
             title: 'Override color',
-            subtitle: 'Override the default theme color. See warning below.',
+            subtitle: 'Override the default theme color.\nWarning: color will no longer adapt to the theme.',
             child: YgTag(
               child: YgIcon(
                 YgIcons.deviceHubChange,
-                color: Colors.green,
+                color: context.tokens.colors.iconCritical,
                 size: YgIconSize.large,
               ),
             ),
           ),
-          YgSection.base(
+          const YgSection.base(
             title: 'Use embedded color',
-            subtitle: 'Color embedded in the SVG is used. See warning below.',
+            subtitle: 'Color embedded in the SVG is used.\nThe color will adapt to the theme.',
             child: YgTag(
               child: YgIcon.embeddedColor(
-                YgIcons.deviceHubColorful,
+                YgIcons.weatherThunderColorful,
                 size: YgIconSize.large,
               ),
             ),
