c02cea1fdb4e6a3c16f8e7b9f990ec4e79b98a8a
emeban44 <44259108+emeban44@users.noreply.github.com>
2023-10-23 14:57:27 +0200

    YgCallout golden tests (#81)
    [dev-improvement] Added YgCallout golden tests.
    [dev-improvement] Added support and created project structure for Golden testing Yggdrassil components. [DEV-2023]

---
+++ test/yg_callout_golden_test.dart
@@ -0,0 +1,108 @@
+import 'package:alchemist/alchemist.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:yggdrasil/yggdrasil.dart';
+
+import 'extensions/_extensions.dart';
+import 'utils/_utils.dart';
+
+void main() {
+  group(
+    'YgCalloutGoldenTest',
+    () {
+      final GoldenTestGroup ygCalloutGoldenTestGroup = GoldenTestGroup(
+        columns: 2,
+        scenarioConstraints: YgGoldenTestValues.scenarioConstraints,
+        children: <GoldenTestScenario>[
+          GoldenTestScenario(
+            name: 'With title, description, onClose and textLink',
+            child: YgCallout(
+              title: YgGoldenTestValues.shortText,
+              description: YgGoldenTestValues.mediumText,
+              onClose: () {},
+              textLink: YgTextLink(
+                text: YgGoldenTestValues.shortText,
+                onPressed: () {},
+              ),
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'With title, description and textLink',
+            child: YgCallout(
+              title: YgGoldenTestValues.shortText,
+              description: YgGoldenTestValues.mediumText,
+              textLink: YgTextLink(
+                text: YgGoldenTestValues.shortText,
+                onPressed: () {},
+              ),
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'With title and description',
+            child: const YgCallout(
+              title: YgGoldenTestValues.shortText,
+              description: YgGoldenTestValues.longText,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'With description only',
+            child: const YgCallout(
+              description: YgGoldenTestValues.longText,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'With long title and description',
+            child: const YgCallout(
+              title: YgGoldenTestValues.longText,
+              description: YgGoldenTestValues.longText,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'With long title and very long description',
+            child: const YgCallout(
+              title: YgGoldenTestValues.longText,
+              description: YgGoldenTestValues.longText,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'With title, description and long textLink',
+            child: YgCallout(
+              title: YgGoldenTestValues.shortText,
+              description: YgGoldenTestValues.mediumText,
+              textLink: YgTextLink(
+                text: YgGoldenTestValues.longText,
+                onPressed: () {},
+              ),
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'With very long title, description and textLink',
+            child: YgCallout(
+              title: YgGoldenTestValues.longText,
+              description: YgGoldenTestValues.longText,
+              textLink: YgTextLink(
+                text: YgGoldenTestValues.longText,
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
+      );
+
+      for (final YgThemeTestData themeTestData in YgGoldenTestValues.themesToTest) {
+        goldenTest(
+          'YgCallout - ${themeTestData.themeName}',
+          fileName: 'yg_callout_${themeTestData.fileName}',
+          builder: () => ygCalloutGoldenTestGroup,
+          pumpWidget: (WidgetTester widgetTester, Widget widget) {
+            return widgetTester.pumpWidgetWithYgTheme(
+              widget: widget,
+              ygTheme: themeTestData.theme,
+            );
+          },
+        );
+      }
+    },
+  );
+}
