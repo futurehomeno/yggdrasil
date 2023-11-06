710b7471c891e7f60ab9fcef0ccc1eaf79f73954
Tim Rots <T99.Rots@gmail.com>
2023-11-06 13:53:12 +0100

    comments

--- lib/src/utils/yg_states/property/yg_driven_property_all.dart
+++ lib/src/utils/yg_states/property/yg_driven_property_all.dart
@@ -9,10 +9,14 @@ class _YgDrivenPropertyAll<V> implements YgDrivenProperty<V> {
   final V value;
 
   @override
-  void addListener(VoidCallback listener) {}
+  void addListener(VoidCallback listener) {
+    // Value will never change.
+  }
 
   @override
-  void removeListener(VoidCallback listener) {}
+  void removeListener(VoidCallback listener) {
+    // Value will never change.
+  }
 
   @override
   YgDrivenProperty<T> map<T>(ValueMapper<V, T> mapper) {
