02e3958c15d1eaa829b172634385c69f74a7de49
Tim Rots <T99.Rots@gmail.com>
2023-11-10 15:16:34 +0100

    better comment

--- example/lib/utils/extensions/enum_list_extension.dart
+++ example/lib/utils/extensions/enum_list_extension.dart
@@ -1,5 +1,9 @@
 extension EnumListExtension<T extends Enum> on List<T> {
-  /// Get the enum that comes after [current].
+  /// Gets the next enum after [current] from a list of enums.
+  ///
+  /// Intended to be used for quickly iterating over Enum.values. [current] has
+  /// to be in the list of enums. Wraps around to the first enum if [current] is
+  /// the last enum in the list.
   T next(T current) {
     final int index = indexOf(current);
     final int nextIndex = (index + 1) % length;
