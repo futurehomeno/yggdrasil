a77bee29e774f7bf6024374fd0f0320249ef3ac3
Tim Rots <T99.Rots@gmail.com>
2023-12-05 13:29:23 +0100

    migrated YgCard

--- lib/src/components/yg_card/yg_card.dart
+++ lib/src/components/yg_card/yg_card.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:yggdrasil/src/theme/_theme.dart';
+import 'package:yggdrasil/src/components/yg_card/yg_card_state.dart';
+import 'package:yggdrasil/src/components/yg_card/yg_card_style.dart';
 import 'package:yggdrasil/src/utils/_utils.dart';
 
 import 'enums/_enums.dart';
-import 'mappers/_mappers.dart';
 
 /// Yggdrasil card component.
 ///
@@ -12,7 +12,7 @@ import 'mappers/_mappers.dart';
 /// By design, this card does not have any padding.
 // TODO(bjhandeland): Make this component stateful or make a stateful card.
 // TODO(bjhandeland): Add support for semanticContainer.
-class YgCard extends StatelessWidget with StatelessWidgetDebugMixin {
+class YgCard extends StatefulWidget {
   const YgCard({
     super.key,
     this.variant = YgCardVariant.elevated,
@@ -26,26 +26,46 @@ class YgCard extends StatelessWidget with StatelessWidgetDebugMixin {
   final Widget? child;
 
   @override
-  Widget build(BuildContext context) {
-    final YgCardTheme cardTheme = context.cardTheme;
+  State<YgCard> createState() => _YgCardState();
+}
 
-    return Material(
-      type: MaterialType.card,
-      color: YgCardMapper.getBackgroundColor(
-        theme: cardTheme,
-        variant: variant,
-      ),
-      shape: YgCardMapper.getShape(
-        theme: cardTheme,
-        variant: variant,
-      ),
-      elevation: YgCardMapper.getElevation(
-        theme: cardTheme,
-        variant: variant,
-      ),
+class _YgCardState extends StateWithYgStyle<YgCard, YgCardStyle> {
+  late final YgCardState _state = YgCardState(
+    variant: widget.variant,
+  );
+
+  @override
+  YgCardStyle createStyle() {
+    return YgCardStyle(
+      state: _state,
+      vsync: this,
+    );
+  }
+
+  @override
+  void didUpdateWidget(covariant YgCard oldWidget) {
+    _state.variant.value = widget.variant;
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    _state.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return YgAnimatedPhysicalShape(
+      shape: style.shape,
+      color: style.backgroundColor,
+      elevation: style.elevation,
       borderOnForeground: false,
       clipBehavior: Clip.antiAlias,
-      child: child,
+      child: Material(
+        type: MaterialType.transparency,
+        child: widget.child,
+      ),
     );
   }
 }
