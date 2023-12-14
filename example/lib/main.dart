7e787735337e491fe20bdc3f04baa2698aa0bbdb
Tim Rots <31472926+T99Rots@users.noreply.github.com>
2023-12-14 16:22:23 +0100

    tim/version-display (#129)
    [improvement] The example app shows the current version in the app bar now.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,7 +11,7 @@ void main() {
 
   runApp(
     ChangeNotifierProvider<YgAppState>(
-      create: (BuildContext context) => YgAppState(),
+      create: (BuildContext context) => YgAppState()..init(),
       child: const Yggdrasil(),
     ),
   );
