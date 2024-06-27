99f7e85bda70567bdaf5bd4a7a93292fb4de4353
Tim Rots <31472926+T99Rots@users.noreply.github.com>
2024-06-27 15:58:08 +0200

    Tim / bar_graph_customizable_values (#173)
    [improvement] Bar graph values now support a value text and metric for each value.

--- yggdrasil/lib/src/components/yg_mini_bar_graph/models/yg_bar_graph_bar.dart
+++ yggdrasil/lib/src/components/yg_mini_bar_graph/models/yg_bar_graph_bar.dart
@@ -1,11 +1,29 @@
 import 'package:yggdrasil/src/components/yg_mini_bar_graph/enums/_enums.dart';
 
+/// A bar rendered in the [YgMiniBarGraph].
 class YgBarGraphBar {
   const YgBarGraphBar({
     this.variant,
     required this.value,
+    required this.valueText,
+    required this.metric,
   });
 
+  /// The variant of the bar.
+  ///
+  /// Primarily determines the color of the bar. If null, a fallback color is
+  /// used instead of a color from the available [BarVariant]s.
   final BarVariant? variant;
+
+  /// The value used for rendering the bar.
+  ///
+  /// This value is not displayed anywhere in text form. Setting the displayed
+  /// text should instead be done using the [valueText].
   final double value;
+
+  /// The value displayed when this bar is the current bar.
+  final String valueText;
+
+  /// The metric displayed when this bar is the current bar.
+  final String metric;
 }
