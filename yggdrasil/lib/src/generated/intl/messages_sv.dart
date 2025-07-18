653d83ac0011f20ddf36c441aa05ec9fd2ca954f
Kamil Szufnara <126680623+kamilszu@users.noreply.github.com>
2025-07-18 15:11:17 +0200

    fixes

--- yggdrasil/lib/src/generated/intl/messages_sv.dart
+++ yggdrasil/lib/src/generated/intl/messages_sv.dart
@@ -24,7 +24,7 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-        "dropdownDone": MessageLookupByLibrary.simpleMessage("Klar"),
-        "wizardHeaderNOfN": m0
-      };
+    "dropdownDone": MessageLookupByLibrary.simpleMessage("Klar"),
+    "wizardHeaderNOfN": m0,
+  };
 }
